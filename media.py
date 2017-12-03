593c949ad6ce0eea94f3a90256c1fc17855b1642
jkolden <john.kolden@gmail.com>
2017-12-03 10:00:22 -0800

    Update media.py

--- media.py
+++ media.py
@@ -11,10 +11,18 @@ class Movie:
     VALID_RATINGS = ["G", "PG", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):  # noqa
+        """
+        This function allows for the creation of a movie object that returns
+        key pieces of information about a given movie
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        This function opens a browser window and displays the youtube 
+        trailer for a given feature
+        """
         webbrowser.open(self.trailer_youtube_url)
